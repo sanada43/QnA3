1347ceb0f5bba2b36328979edffa349a941be43f
sanada43 <sakurada@oce.co.jp>
2018-08-31 14:44:15 +0900

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=testbot-sakurada-Web-Deploy.pubxml -p:Password=bz3hSjhoMAcksktXfolS0GGfELkmgktCjgZ06hfjddxaa7TGrsjg6uEq9uFh
+
